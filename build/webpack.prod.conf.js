f99291484d8eedc70593323078b7a5bb1c7107df
vasily.molostov <vasily.molostov@ITW-003.lab.gemotest.ru>
2018-08-15 17:31:57 +0300

    update title names for webpack and npm

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -10,8 +10,8 @@ var webpackConfig = merge(baseWebpackConfig, {
   devtool: '#source-map',
   output: {
     path: config.build.assetsRoot,
-    filename: 'vue-table-with-tree-grid.js',
-    library: 'vue-table-with-tree-grid',
+    filename: 'vue-treetable-grid.js',
+    library: 'vue-treetable-grid',
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
